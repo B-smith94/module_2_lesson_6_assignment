b525e2697aa5853ecbcdde4dc51cb26c5ad406f8
B-smith94 <byrondsmith9@gmail.comcleargit config --global user.email byrondsmith9@gmail.comssh-keygen -t rsa -b 4096 -C byrondsmith9@gmail.com>
2024-08-06 17:28:33 -0400

    Set script to iterate over substrings in each string in the reviews list

--- product_review_analysis.py
+++ product_review_analysis.py
@@ -1,25 +1,4 @@
 #Task 1:
-
-
-def review_analysis(review_list):
-    joined_reviews = " ".join(review_list)
-    caps_words = []
-    for review in review_list:
-        for word in review:
-            if word == "good":
-                caps_words.append("GOOD")
-            elif word == "bad":
-                caps_words.append("BAD")
-            elif word == "poor":
-                caps_words.append("POOR")
-            elif word == "excellent":
-                caps_words.append("EXCELLENT") 
-            elif word == "average":
-                caps_words.append("AVERAGE")
-            else:
-                caps_words.append(word)
-    return ''.join(caps_words)
-
 reviews = [
         "This product is really good. I'm impressed with its quality.",
         "The performance of this product is excellent. Highly recommended!",
@@ -27,8 +6,11 @@ reviews = [
         "Poor quality product. Wouldn't recommend it to anyone.",
         "The product was average. Nothing extraordinary about it."
     ]
-
-print(review_analysis(reviews))
+rating = ['good', 'bad', 'excellent', 'poor', 'average']
+for review in reviews:
+    for substring in review:
+        print(substring)
+        
     
 #Task 2:
 
