092076098b6ea8889a992181632e49c6647db7dd
b-smith94 <byrondsmith9@gmail.com>
2024-08-06 19:20:56 -0400

    restart

--- product_review_analysis.py
+++ product_review_analysis.py
@@ -1,25 +1,4 @@
 #Task 1:
-
-
-def review_analysis(review_list):
-    joined_reviews = " ".join(review_list)
-    caps_words = []
-    for review in review_list:
-        for word in review:
-            if word == "good":
-                caps_words.append("GOOD")
-            elif word == "bad":
-                caps_words.append("BAD")
-            elif word == "poor":
-                caps_words.append("POOR")
-            elif word == "excellent":
-                caps_words.append("EXCELLENT") 
-            elif word == "average":
-                caps_words.append("AVERAGE")
-            else:
-                caps_words.append(word)
-    return ''.join(caps_words)
-
 reviews = [
         "This product is really good. I'm impressed with its quality.",
         "The performance of this product is excellent. Highly recommended!",
@@ -28,7 +7,12 @@ reviews = [
         "The product was average. Nothing extraordinary about it."
     ]
 
-print(review_analysis(reviews))
+keyword = ["good", "bad", "poor", "excellent", "average"]
+
+result = [word for word in reviews if keyword[0:] in word]
+print(result)
+
+
     
 #Task 2:
 
