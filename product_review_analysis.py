bfc33d3b368395ba57c614dcbc5ad790cf398f13
b-smith94 <byrondsmith9@gmail.com>
2024-08-07 10:42:31 -0400

    completed task 3

--- product_review_analysis.py
+++ product_review_analysis.py
@@ -26,9 +26,9 @@ for review in reviews:
         else:
             buzzwords.append(word)
         
-#    review_no_period = ' '.join(buzzwords)
-#    analyzed_reviews = review_no_period[:index] + "." + review_no_period[index:]
-#    individual_reviews = '--' + analyzed_reviews[0:]
+    review_no_period = ' '.join(buzzwords)
+    analyzed_reviews = review_no_period[:index] + "." + review_no_period[index:]
+    individual_reviews = '--' + analyzed_reviews[0:]
 #    print(individual_reviews)
 
 #Task 2:
@@ -36,14 +36,27 @@ positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb"
 negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
 
 def sentiment_tally(positive, negative):
-    if positive:
-        total_positive = len(positive)
-        print(f"Total number of positive words: {total_positive}")
-    elif negative:
-        total_negative = len(negative)
-        print(f"Total number of negative words: {total_negative}")
+    total_positive = len(positive)
+    print(f"Total number of positive words: {total_positive}")
 
-sentiment_tally(positive_words, negative_words)
+    total_negative = len(negative)
+    print(f"Total number of negative words: {total_negative}")
+
+#sentiment_tally(positive_words, negative_words)
 
 #Task 3:
-
+for review in reviews:
+    if review[:30].endswith(" "):
+        summary = review[:30] + "..."
+        print(summary)
+    elif review[:31].endswith(" "):
+        summary = review[:31] + "..."
+        print(summary)
+    elif review[:32].endswith(" "):
+        summary = review[:32] + "..."
+        print(summary)
+    elif review[:33].endswith(" "):
+        summary = review[:33] + "..."
+        print(summary)
+    else:
+        break
