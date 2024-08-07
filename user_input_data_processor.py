4e441555391ae36305cc4d1db7a8d74aad4f6f62
b-smith94 <byrondsmith9@gmail.com>
2024-08-07 14:40:15 -0400

    Reworded the error message

--- user_input_data_processor.py
+++ user_input_data_processor.py
@@ -4,7 +4,8 @@ last_name = input("Please enter your last name: ")
 full_name = first_name + last_name
 
 if len(first_name) < 3 or len(last_name) < 3:
-    print("ERROR: Your name must be more than 2 characters.")    
+    print("ERROR: Your first and last names must be more than 2 characters each.")    
 else: 
     print(f"Your name has {len(full_name)} characters.")
        
+    
